da2acd93649df25c1b8fed46167256bb1ff95c7e
jeff5m <jeffmatheuspereira@gmail.com>
2021-08-27 22:52:46 -0300

    add HomePage

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:notes/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Notes",
-      home: Container(
-        color: Colors.deepPurple,
-      ),
+      home: HomePage(),
     );
   }
 }
