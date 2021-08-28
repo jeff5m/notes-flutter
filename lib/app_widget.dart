a3dc79437182ba82a2d3c3bdd39e84cc3ec5f562
jeff5m <jeffmatheuspereira@gmail.com>
2021-08-27 22:38:35 -0300

    initial project structure

---
+++ lib/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Notes",
+      home: Container(
+        color: Colors.deepPurple,
+      ),
+    );
+  }
+}
